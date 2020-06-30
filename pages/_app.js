5cd031600917865cf054e19a684e1abf23585842
Andy Swinburne <andyswinburne@hotmail.com>
2020-06-30 21:59:40 +0100

    Split app and index page

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,5 @@
-import Head from 'next/head';
-import Layout from '../components/Layout';
 import '../scss/main.scss';
 
-const Home = () => (
-    <Layout>
-        <Head>
-            <title>Home | Swinburne Dev</title>
-            <meta name="description" content="Andy Swinburne dev playground site" />
-        </Head>
-        <h1 data-text="Swinburne">Swinburne</h1>
-        <h2 data-text="Development">Development</h2>
-    </Layout>)
-
-export default Home;
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
