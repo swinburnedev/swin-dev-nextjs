47871eba921b07264870bf22960585ced20c9b2e
Andy Swinburne <andyswinburne@hotmail.com>
2020-07-01 22:06:33 +0100

    Add custom document and increase colour contrast

--- components/Layout.js
+++ components/Layout.js
@@ -3,10 +3,6 @@ import Head from 'next/head';
 const Layout = ({children}) => (
     <React.Fragment>
         <Head>
-            <link rel="preload" href="static/apple-touch-icon.png" as="image" />
-            <link rel="preload" href="static/favicon-32x32.png" as="image" />
-            <link rel="preload" href="static/favicon-16x16.png" as="image" />
-            <link rel="preload" href="static/site.webmanifest" as="image" />
             <link rel="apple-touch-icon" sizes="180x180" href="static/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="static/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="static/favicon-16x16.png" />
