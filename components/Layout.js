498f31aba86054709cbfcdcd3f648149b59d79aa
Andy Swinburne <andyswinburne@hotmail.com>
2020-07-01 20:27:56 +0100

    Add favicon

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,21 @@
+import Head from 'next/head';
+
 const Layout = ({children}) => (
-    <main>
-        { children }
-    </main>
+    <React.Fragment>
+        <Head>
+            <link rel="preload" href="static/apple-touch-icon.png" as="image" />
+            <link rel="preload" href="static/favicon-32x32.png" as="image" />
+            <link rel="preload" href="static/favicon-16x16.png" as="image" />
+            <link rel="preload" href="static/site.webmanifest" as="image" />
+            <link rel="apple-touch-icon" sizes="180x180" href="static/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="static/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="static/favicon-16x16.png" />
+            <link rel="manifest" href="static/site.webmanifest" />
+        </Head>
+        <main>
+            { children }
+        </main>
+    </React.Fragment>
 )
 
 export default Layout;
